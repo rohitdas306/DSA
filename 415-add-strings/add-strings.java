0426a82268480a22c9c1117eabf67e3651ed6cdc
ROHIT KUMAR DAS <65764852+rohitdas306@users.noreply.github.com>
2025-02-15 20:40:33 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (74.05%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,9 +1,19 @@
-import java.math.BigInteger;
+
 class Solution {
     public String addStrings(String num1, String num2) {
-         BigInteger number1 = new BigInteger(num1);
-        BigInteger number2 = new BigInteger(num2);
-        return number1.add(number2).toString();
+        StringBuilder result = new StringBuilder();
+        int carry = 0, i = num1.length() - 1, j = num2.length() - 1;
+        
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) sum += num1.charAt(i--) - '0';
+            if (j >= 0) sum += num2.charAt(j--) - '0';
+            
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+        
+        return result.reverse().toString();
      }
     
     }

2b117ffe5895e246fe82ccaefa493fae48009a6e
ROHIT KUMAR DAS <65764852+rohitdas306@users.noreply.github.com>
2025-02-16 16:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (30.40%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String sortSentence(String s) {
+        String words[]=s.split(" ");
+        String sortedWords[] = new String[words.length];
+
+        for(String word: words){
+            int position = word.charAt(word.length() - 1) - '1';
+            sortedWords[position] = word.substring(0, word.length() - 1);
+        }
+        return String.join(" ", sortedWords);
+    }
+}
